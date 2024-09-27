d3cbdcfd1ce58dea171ce972e9751f9033e220ad
AShu040390 <vaibhavnagar.nagar@gmail.com>
2024-09-27 17:34:03 +0530

    Create credentials.ts
    Credentials File

---
+++ tests/credentials.ts
@@ -0,0 +1,10 @@
+export const credentials = {
+    loginId: 'vaibhavnagar.nagar@gmail.com',
+    password: 'AShu@0403',
+    name: 'John Doe',
+    company: 'Acme Corp', 
+    lname: 'Doe', 
+    email: 'john.doe@acme.com', 
+    newEmail: 'john.doe@newemail.com',
+    phone: '1234567890',
+  };
